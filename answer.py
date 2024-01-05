e2e073984cbfa8d022b98dfbd28043dcb3d8cad6
e9srawat <shivansh.rawat@enine.school>
2024-01-05 16:01:22 +0530

    Assignment Completed

--- answer.py
+++ answer.py
@@ -1,2 +1,7 @@
+"""Math 020"""
+from solver import solver
+
+
 def answer():
-    return
+    """returns sum of the digits in 100!"""
+    return solver(100)
--- solver.py
+++ solver.py
@@ -1,2 +1,26 @@
-def solver():
-    return
+"""Math 020"""
+
+
+def solver(n: int):
+    """returns sum of the digits in n!"""
+    factn = 1
+    for i in range(1, n + 1):
+        carry = 0
+        prd = 0
+        prdl = []
+        for j in reversed(range(len(str(factn)))):
+            if carry:
+                prd = int(str(factn)[j]) * i + int(carry)
+            else:
+                prd = int(str(factn)[j]) * i
+            if j == 0:
+                prdl.append(str(prd))
+            else:
+                prdl.append(str(prd)[-1])
+                carry = str(prd)[0:-1]
+        factn = "".join(reversed(prdl))
+        addr = 0
+    for i in factn:
+        addr += int(i)
+
+    return addr
